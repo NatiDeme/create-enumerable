af699febca5b5188e1c3033e988737ec9f46c45a
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-08-19 00:27:02 +0430

    Add MyList file

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,21 +1,21 @@
 module MyEnumerable
     def all?
-      each do |el|
-        return false unless yield(el)
+      each do |element|
+        return false unless yield(element)
       end
       true
     end
     def any?
-        each do |el|
-          return true if yield(el)
+        each do |element|
+          return true if yield(element)
         end
         false
       end
     
     #   def filter
     #     array = []
-    #     each do |el|
-    #       array << el if yield(el)
+    #     each do |element|
+    #       array << element if yield(element)
     #     end
     #     array
     #   end
